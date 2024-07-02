e0ffd42f61380b639b4b88f08e4596529b3c116f
unknown <maroonever96@gmail.com>
2024-07-02 17:11:40 +0900

    phone book and dom example

---
+++ day6/dom.ts
@@ -0,0 +1,9 @@
+let physicalDOM = document.createElement("p");
+physicalDOM.innerText = 'Hello physicalDOM world!';
+
+document.body.appendChild(physicalDOM);
+
+let spanPhysicalDOM = document.createElement("span");
+spanPhysicalDOM.innerText = "span test"
+
+document.body.appendChild(spanPhysicalDOM);
