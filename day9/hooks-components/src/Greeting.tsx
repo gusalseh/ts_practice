c9c5974e136c927365c60b2a188e3d8bfe926c2d
unknown <maroonever96@gmail.com>
2024-07-10 10:41:36 +0900

    add Greeting.tsx

---
+++ day9/hooks-components/src/Greeting.tsx
@@ -0,0 +1,29 @@
+import React, {FC, useState, useEffect} from "react";
+
+interface GreetingProps {
+  name?: string
+}
+const Greeting: FC<GreetingProps> =(
+  { name }: GreetingProps
+) => {
+  // message: 내가 만든 변수의 초기값
+  // setMessage: message값을 변경할 때 사용하는 함수
+  const [message, setMessage] = useState("");
+
+  // useEffect: name 변수가 바뀔 때 마다,
+  // if 내부의 값으로 바뀜
+  // setter 개념과 유사
+  useEffect(() => {
+    if (name) {
+      setMessage(`Hello from, ${name}`);
+    }
+  }, [name])
+
+  if (!name) {
+    return <div>no name given</div>;
+  }
+
+  return <div>{message}</div>
+}
+
+export default Greeting;
