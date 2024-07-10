4af42d5a11e417db12e1316d3be65b00e7f5b8be
unknown <maroonever96@gmail.com>
2024-07-10 11:06:31 +0900

    update App.tsx

--- day9/hooks-components/src/App.tsx
+++ day9/hooks-components/src/App.tsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './GreetingFunctional';
+
+const reducer = (state: any, action: any) => {
+  console.log("enterednameReducer");
+  switch (action.type) {
+    case "enteredName":
+      if(state.enteredName === action.payload) {
+        return state;
+      }
+      return {...state, enteredName: action.payload}
+      
+    case "message":
+      return {
+        ...state, message: `Hello, ${action.payload}`
+      }
+    default:
+      throw new Error("Invalid action type " + action.type);
+  }
+}
+
+const initialState = {
+  enteredName: "",
+  message: "",
+}
 
 function App() {
+  const [{ message, enteredName }, dispatch] = useReducer(reducer, initialState);
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: "enteredName", payload: e.target.value
+    });
+    dispatch({
+      type: "message", payload: e.target.value
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input value={enteredName} onChange={onChangeName} />
+        <Greeting message={message} />
       </header>
     </div>
   );
