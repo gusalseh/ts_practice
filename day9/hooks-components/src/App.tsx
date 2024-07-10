c4a6db39bfdcad437b9dec83ca5f52fd2bf46039
unknown <maroonever96@gmail.com>
2024-07-10 11:32:31 +0900

    add increment service

--- day9/hooks-components/src/App.tsx
+++ day9/hooks-components/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { useReducer } from 'react';
+import React, { useCallback, useReducer, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greeting from './GreetingFunctional';
+import { start } from 'repl';
 
 const reducer = (state: any, action: any) => {
   console.log("enterednameReducer");
@@ -28,12 +29,39 @@ const initialState = {
 
 function App() {
   const [{ message, enteredName }, dispatch] = useReducer(reducer, initialState);
+  const [startCount, setStartCount] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const setCountCallback = useCallback(() => {
+    const inc = count + 1 > startCount ? count + 1 :
+      Number(count + 1) + startCount;
+    setCount(inc);
+  }, [count, startCount]);
+
+  const onWelcomeBtnClick = () => {
+    setCountCallback();
+  }
+
+  const onChangeStartCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStartCount(Number(e.target.value));
+  }
+
+  console.log("App.tsx render")
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className='App-logo' alt='logo'/>
+
         <Greeting message={message} enteredName={enteredName} greetingDispatcher={dispatch} />
+        <div style={{marginTop: '10px'}}>
+          <label>Enter a number and we'll increment it</label>
+          <br/>
+          <input value={startCount} onChange={onChangeStartCount} style={{width: '.75rem'}}/>&nbsp;
+          <label>{count}</label>
+          <br/>
+          <button onClick={onWelcomeBtnClick}>Increment count</button>
+        </div>
       </header>
     </div>
   );
