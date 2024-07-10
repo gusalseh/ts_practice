b4171b70d8cdfe081daed07ef037df33266b5eb2
unknown <maroonever96@gmail.com>
2024-07-10 11:43:05 +0900

    add initial components tags

---
+++ day9/hooks/src/UseStateApp1.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function UseStateApp1() {
+  return (
+    <div>
+      <span>현재시각: </span>
+      <button>Update</button>
+    </div>
+  )
+}
+
+export default UseStateApp1;
