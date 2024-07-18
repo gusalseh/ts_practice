f680cc1fc2865364ccdfa029679feb82004aff28
unknown <maroonever96@gmail.com>
2024-07-18 16:38:43 +0900

    change all var to let

--- day14/graphql-schema/server3.js
+++ day14/graphql-schema/server3.js
@@ -3,24 +3,20 @@ let { createHandler } = require("graphql-http/lib/use/express");
 let { buildSchema } = require("graphql");
 
 // Construct a schema, using GraphQL schema language
-let schema = buildSchema(`
+let schema = buildSchema(/* GraphQL */ `
   type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 let root = {
-  quoteOfTheDay() {
-    return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
-  },
-  random() {
-    return Math.random();
-  },
-  rollThreeDice() {
-    return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
+  rollDice({ numDice, numSides }) {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
   },
 };
 
