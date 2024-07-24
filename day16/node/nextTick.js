354f5d4bf7731b225521e6d5dfe945b59c919c25
unknown <maroonever96@gmail.com>
2024-07-24 14:26:06 +0900

    process nextTick example

---
+++ day16/node/nextTick.js
@@ -0,0 +1,18 @@
+// 4
+setImmediate(() => {
+  console.log("immediate");
+});
+
+// 1
+// nextTick의 콜백 함수를 우선적으로 처리
+process.nextTick(() => {
+  console.log("nextTick");
+});
+
+// 3
+setTimeout(() => {
+  console.log("timeout");
+}, 0);
+
+// 2
+Promise.resolve().then(() => console.log("promise"));
