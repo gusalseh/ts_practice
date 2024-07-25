60809543be4460dca300d4b9b8c7a1932f96ecbd
unknown <maroonever96@gmail.com>
2024-07-25 10:51:26 +0900

    only one catch

--- day17/fs/asyncOrder.js
+++ day17/fs/asyncOrder.js
@@ -7,22 +7,14 @@ fs.readFile("./readme2.txt")
   .then((data) => {
     console.log("1번", data.toString());
     // 2번 시작
-    fs.readFile("./readme2.txt")
-      .then((data) => {
-        console.log("2번", data.toString());
-        // 3번 시작
-        fs.readFile("./readme2.txt")
-          .then((data) => {
-            console.log("3번", data.toString());
-            console.log("끝");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      })
-      .catch((err) => {
-        console.log(err);
+    fs.readFile("./readme2.txt").then((data) => {
+      console.log("2번", data.toString());
+      // 3번 시작
+      fs.readFile("./readme2.txt").then((data) => {
+        console.log("3번", data.toString());
+        console.log("끝");
       });
+    });
   })
   .catch((err) => {
     console.log(err);
