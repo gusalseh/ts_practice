e3b0dcf3fb0deff9f8484dd1ee24bc148e08334d
unknown <maroonever96@gmail.com>
2024-07-25 12:24:31 +0900

    copy file

---
+++ day17/fs/copyFile.js
@@ -0,0 +1,11 @@
+const { error } = require("console");
+
+const fs = require("fs").promises;
+
+fs.copyFile("readme4.txt", "writeme4.txt")
+  .then(() => {
+    console.log("복사 완료");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
