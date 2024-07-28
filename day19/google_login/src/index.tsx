a31f8a79213b2320d19ba5f7c1f7b337d39f1b81
gusalseh <maroonever96@gmail.com>
2024-07-28 19:52:17 +0900

    add googleOAuthProvider

--- day19/google_login/src/index.tsx
+++ day19/google_login/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.Fragment>
-    <App />
+    <GoogleOAuthProvider clientId="maroonever">
+      <App />
+    </GoogleOAuthProvider>
   </React.Fragment>
 );
