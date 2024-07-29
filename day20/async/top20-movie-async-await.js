fca948cba9f0dea05622a4c3f296d236b3c1ad25
unknown <maroonever96@gmail.com>
2024-07-29 15:41:56 +0900

    top20 movie async await

---
+++ day20/async/top20-movie-async-await.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+
+async function getTop20Movies() {
+  const url = "https://raw.githubusercontent.com/wapj/jsbackend/main/movieinfo.json"
+
+  try {
+    const result = await axios.get(url);
+    const { data } = result;
+
+    if(!data.articleList || data.articleList.size == 0) {
+      throw new Error("데이터가 없습니다.");
+    }
+
+    const movieInfos = data.articleList.map(
+      (article, idx) => {
+        return { title: article.title, rank: idx + 1 };
+      }
+    )
+    
+    for (let movieInfo of movieInfos) {
+      console.log(`[${movieInfo.rank}위] ${movieInfo.title}`)
+    }
+
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+getTop20Movies()
