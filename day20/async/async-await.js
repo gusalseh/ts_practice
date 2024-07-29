4e9bbf890278487d005f86e8938f09876eda9384
unknown <maroonever96@gmail.com>
2024-07-29 15:36:13 +0900

    count one to ten by async

--- day20/async/async-await.js
+++ day20/async/async-await.js
@@ -8,3 +8,19 @@ async function showName() {
 }
 
 console.log(showName())
+
+function waitOneSecond(msg) {
+  return new Promise((resolve, _) => {
+    setTimeout(() => resolve(`${msg}`), 1000);
+  });
+}
+
+async function countOneToTen() {
+  for (let x of [...Array(10).keys()]) {
+    let result = await waitOneSecond(`${x + 1}초 대기 중...`)
+    console.log(result)
+  }
+  console.log("완료")
+}
+
+countOneToTen();
